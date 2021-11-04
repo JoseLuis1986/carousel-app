8355e1771e0cbd2419a48bfa0d8ac5abd6661c0d
Jose Luis <joseluissanchezgarcia1986@gmail.com>
2021-11-04 09:03:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Carousel from 'react-bootstrap/Carousel';
+import { slides } from './slides';
 
-function App() {
+
+
+export const App = () => {
+
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'block', width: 800, margin: 'auto', paddingTop: 150 }}>
+      <h4>React-Bootstrap Carousel Component</h4>
+      <Carousel activeIndex={index} onSelect={handleSelect} style={{ height: 560 }}>
+        {
+          slides.map((item, i) => (
+            <Carousel.Item interval={3000}>
+              <img
+                className="d-inline-block w-100"
+                src={item.url}
+                alt={item.alt}
+              />
+              <Carousel.Caption>
+                <h3>{item.title}</h3>
+                <p>{item.text}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))
+        }
+      </Carousel>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
+// return (
+//   <div>
+//     <Carousel activeIndex={index} onSelect={handleSelect}>
+//       <Carousel.Item>
+//         <img
+//           className="d-block w-100"
+//           src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+//           alt="First slide"
+//         />
+//         <Carousel.Caption>
+//           <h3>First slide label</h3>
+//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <img
+//           className="d-block w-100"
+//           src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+//           alt="Second slide"
+//         />
+
+//         <Carousel.Caption>
+//           <h3>Second slide label</h3>
+//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <img
+//           className="d-block w-100"
+//           src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+//           alt="Third slide"
+//         />
+
+//         <Carousel.Caption>
+//           <h3>Third slide label</h3>
+//           <p>
+//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//           </p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
+//   </div>
+// )
---
+++ src/slides.js
@@ -0,0 +1,20 @@
+export const slides = [
+    {
+        url: "https://ep00.epimg.net/elpais/imagenes/2019/10/30/album/1572424649_614672_1572451731_album_normal.jpg",
+        alt: "one",
+        title: "Label for first slide",
+        text: "Sample Text for Image One"
+    },
+    {
+        url: "https://ep01.epimg.net/elpais/imagenes/2019/10/30/album/1572424649_614672_1572453030_noticia_normal.jpg",
+        alt: "two",
+        title: "Label for second slide",
+        text: "Sample Text for Image two"
+    },
+    {
+        url: "https://ep00.epimg.net/elpais/imagenes/2019/10/30/album/1572424649_614672_1572452580_album_normal.jpg",
+        alt: "three",
+        title: "Label for third slide",
+        text: "Sample Text for Image Three"
+    },
+]
